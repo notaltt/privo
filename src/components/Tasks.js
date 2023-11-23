006f0840902ad744aed6d3f0c8608d64b50c09db
slashplusdash <rreforma39@gmail.com>
2023-11-24 01:14:03 +0800

    Task.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import SideBar from './SideBar';
 import Profile from './Profile-Menu';
 import DarkMode from './DarkMode';
@@ -6,9 +6,8 @@ import {generateDate, months } from '../components-additional/GenerateDate';
 import dayjs from "dayjs";
 import cn from '../components-additional/cn'
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
-import TeamSelector from '../components-additional/TeamSelector';
-import { firestore as db  } from './firebase';
-import { getDoc, doc, collection, getDocs, query, where } from 'firebase/firestore';
+import { firestore as db } from './firebase'; 
+import { getDoc, doc, collection, getDocs, query, where, addDoc } from 'firebase/firestore';
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from '../../src/components/firebase';
 
@@ -22,84 +21,195 @@ function Tasks({ user }) {
   const [currentUser, setCurrentUser] = useState();
   const [userCompany, setUserCompany] = useState();
   const [isLoading, setIsLoading] = useState(true);
-  const [joinedTeams, setJoinedTeams] = useState();
+  const [joinedTeams, setJoinedTeams] = useState([]);
+  const [selectedTeam, setSelectedTeam] = useState('');
+  const [isManager, setIsManager] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+  const [userRole, setUserRole] = useState('');
+  const [users, setUsers] = useState([]);
+  const [selectedUserEmail, setSelectedUserEmail] = useState('');
+  const [taskName, setTaskName] = useState('');
+  const [taskDate, setTaskDate] = useState('');
+  const [taskDescription, setTaskDescription] = useState('');
+  const [hasFetched, setHasFetched] = useState(false);
+  const [team, setTeam] = useState();
 
- 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        fetchTeam(user);
-        setCurrentUser(user);
-        getUserCompany(user); 
-      } else {
-        setCurrentUser();
-        setUserCompany(); // Reset userCompany when not authenticated
-      }
-    });
-
-    return () => unsubscribe();
-  }, [userCompany]);
-
-
-
-  const fetchTeam = async (user) => {
-    const teams = [];
-    try {
-    const userRef = doc(db, 'users', user.uid);
-    const userSnapshot = await getDoc(userRef);
-
-    if (userSnapshot.exists()) {
-        const userData = userSnapshot.data();
-        const userTeams = userData.teams || [];
-
-        const teamRef = collection(db, 'team');
-        const teamQuery = query(teamRef, where('teamName', 'array-contains-any', userTeams));
-        const teamSnapshot = await getDocs(teamQuery);
-
-        teamSnapshot.forEach((doc) => {
-        const teamData = doc.data();
-        const members = teamData.members || [];
-        const totalMembers = members.length;
-
-        teams.push({ id: doc.id, ...teamData, totalMembers });
-        });
-    }
-    } catch (error) {
-    console.error("Error fetching team:", error);
-    }
-    setJoinedTeams(teams);
-};
-
-const member = (length) => {
-    return length === 1 ? " member" : " members";
-};
-
-  const getUserCompany = async (user) => {
-  try {
-      const userRef = doc(db, 'users', user.uid);
-      const userUid = await getDoc(userRef);
-
-      if (userUid.exists) {
-          const userData = userUid.data();
-          const userCompany = userData.company;
-          setUserCompany(userCompany);
-          setIsLoading(false);
-      } else {
-          console.log('User document not found');
-          setUserCompany();
-      }
-      } catch (error) {
-          console.error('Error fetching user data:', error);
-          setUserCompany();
-      }
+  const handleUserChange = (event) => {
+    setSelectedUserEmail(event.target.value);
   };
 
+   useEffect(() => {
+    const fetchData = async (user) => {
+      if (user) {
+        getUser(user);
+        fetchTeam(user);
+        fetchUsers(user);
+        checkUserRole(user);
+        setHasFetched(true);
+        setIsLoading(false); 
+      } else {
+        console.log("User is not authenticated.");
+      }
+    };
+  
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!hasFetched) {
+        setCurrentUser(user);
+        fetchData(user);
+      }
+    });
+  
+    return () => unsubscribe();
+  }, [hasFetched]);
+  
+  
+  const getUser = async (user) => {
+    try {
+      const userData = doc(db, 'users', user.uid);
+      const userDoc = await getDoc(userData);
+  
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        const userAvatar = userData.avatar;
+        const userName = userData.name;
+        const userRole = userData.role;
+  
+        setUserName(userName);
+        setUserAvatar(userAvatar);
+        setUserRole(userRole);
+      }
+    } catch (e) {
+      console.error("Error fetching user data:", e);
+    }
+  };
+  
+  const checkUserRole = async (user) => {
+    try {
+      const userDocRef = doc(db, "users", user.uid);
+      const userDocSnapshot = await getDoc(userDocRef);
+  
+      if (userDocSnapshot.exists()) {
+        const userData = userDocSnapshot.data();
+        const userRole = userData.role;
+  
+        if (userRole === "manager") {
+          setIsManager(true);
+          console.log(userRole);
+          console.log("User is a manager");
+        } else if (userRole === "member") {
+          setIsManager(false);
+          console.log(userRole);
+          console.log("User is a member");
+        }
+      }
+    } catch (error) {
+      console.error("Error checking user role:", error);
+    }
+  };
+  
+  const fetchTeam = async (user) => {
+    try {
+      const userRef = doc(db, 'users', user.uid);
+      const userSnapshot = await getDoc(userRef);
+  
+      if (userSnapshot.exists()) {
+        const userData = userSnapshot.data();
+        const userCompany = userData.company;
+  
+        const teamRef = collection(db, "team");
+        const queryCompany = query(teamRef, where('fromCompany', '==', userCompany));
+        const companySnapshot = await getDocs(queryCompany);
+  
+        const teams = companySnapshot.docs.map((companyDoc) => {
+          const companyData = companyDoc.data();
+          const members = companyData.members || [];
+          const totalMembers = members.length;
+  
+          return { id: companyDoc.id, ...companyData, totalMembers };
+        });
+  
+        setJoinedTeams(teams); // Update the state variable to hold the fetched teams
+        console.log("AVAILABLE", teams);
+      } else {
+        console.log("User snapshot does not exist.");
+      }
+    } catch (error) {
+      console.error("Error fetching team:", error);
+    }
+  };
+  
+ 
+  const fetchUsers = async (user, selectedTeam) => {
+    try {
+      const userRef = doc(db, 'users', user.uid);
+      const userSnapshot = await getDoc(userRef);
+  
+      if (userSnapshot.exists()) {
+        const userData = userSnapshot.data();
+        const userCompany = userData.company;
+  
+        const companyRef = collection(db, 'users');
+        const companyQuery = query(companyRef, where('company', '==', userCompany));
+        const companySnapshot = await getDocs(companyQuery);
+  
+        const users = companySnapshot.docs
+          .map((doc) => doc.data())
+          .filter((user) => user.teams && user.teams.includes(selectedTeam));
+  
+        setUsers(users);
+        console.log(users);
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  const addTask = async () => {
+    try {
+      if (!selectedTeam || !taskName || !taskDate || !selectedUserEmail || !taskDescription) {
+        console.error('Incomplete task details. Please fill in all fields.');
+        return;
+      }
+  
+      const tasksCollection = collection(db, 'tasks');
+  
+      await addDoc(tasksCollection, {
+        team: selectedTeam,
+        taskName: taskName,
+        date: taskDate,
+        assignedUser: selectedUserEmail,
+        description: taskDescription,
+      });
+  
+      console.log('Task added successfully!');
+      closeModal();
+    } catch (error) {
+      console.error('Error adding task:', error);
+    }
+  };
+  
+
+  
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-
-  const openModal = () => {
+  const openModal = (task) => {
+    // Check if task data exists
+    if (task) {
+      // Set initial values for the task-related states
+      setTaskName(task.taskName || '');
+      setTaskDate(task.date || '');
+      setSelectedUserEmail(task.assignedUser || '');
+      setTaskDescription(task.description || '');
+    } else {
+      // If no task data, set default values or clear the fields
+      setTaskName('');
+      setTaskDate('');
+      setSelectedUserEmail('');
+      setTaskDescription('');
+    }
     setIsModalOpen(true);
   };
 
@@ -113,6 +223,8 @@ const member = (length) => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+
+
   return(
   <div className="flex dark:bg-gray-950 bg-white">           
     <SideBar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
@@ -121,31 +233,42 @@ const member = (length) => {
         <div className="flex md:justify-center flex-1 lg:mr-32">
               <div className=" relative w-40 justify-center md:w-full max-w-xl mr-6 focus-within:text-purple-500">
               <div>
-                <select
-                  className="w-full pl-8 pr-2 text-large dark:text-black text-black placeholder-blue-600 bg-gray-200 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-blue dark:focus:placeholder-gray-600 
-                  dark:bg-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-red-300 focus:outline-none focus:shadow-outline-purple focus:text-blue-500 form-input"
-                  aria-label="Choose Team"
-                  defaultValue=""
-                  id="team-select"
-                >
-                  {joinedTeams && joinedTeams.length > 0 ? (
-                        joinedTeams.map((team) => (
-                          <option key={team.id}>
-                            {team.teamName}
-                          </option>
-                        ))
-                  ): (
-                    <div>
-                        No teams available or you're not part of any teams.
-                    </div>
-                  )}
-                </select>
+              <select
+                className="w-full pl-8 pr-2 text-large dark:text-black text-black placeholder-blue-600 bg-gray-200 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-blue dark:focus:placeholder-gray-600 
+                dark:bg-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-red-300 focus:outline-none focus:shadow-outline-purple focus:text-blue-500 form-input"
+                aria-label="Choose Team"
+                value={selectedTeam}
+                onChange={(e) => {
+                  const selectedId = e.target.value;
+                  const team = joinedTeams.find((team) => team.id === selectedId);
+                  setSelectedTeam(team ? team.id : ''); // Set the selected team ID
+                  fetchUsers(currentUser); // Fetch users for the selected team
+                }}
+                id="team-select"
+              >
+              {joinedTeams && joinedTeams.length > 0 ? (
+                joinedTeams.map((team) => (
+                  <option key={team.id} value={team.id}>
+                    {team.teamName}
+                  </option>
+                ))
+              ) : (
+                <div>
+                  No teams available or you're not part of any teams.
+                </div>
+              )}
+              </select>
             </div>
             </div> 
             <div>
-            <button className="mt-0 ml-5 mr-5 gap-10 h-12 w-32 flex-none rounded-full bg-sky-300 hover:bg-cyan-200 me-4 font-semibold" onClick={openModal}>
-              Add Task
-            </button>
+            {isManager && ( // This will only render the button if isManager is true
+              <button
+                className="mt-0 ml-5 mr-5 gap-10 h-12 w-32 flex-none rounded-full bg-sky-300 hover:bg-cyan-200 me-4 font-semibold"
+                onClick={openModal}
+              >
+                Add Task
+              </button>
+            )}
             </div>
             <div className='mt-2 position-absolute right-0'>
               <DarkMode/>
@@ -263,13 +386,8 @@ const member = (length) => {
         {isModalOpen && (
           <div id="modal" className="fixed top-0 left-0 w-full h-full bg-opacity-80 bg-gray-900 flex justify-center items-center">
             <div className="bg-white dark:text-white dark:bg-gray-500 rounded-lg shadow-lg p-12">
-              <div class="aa de dn md aue avb bxo">
-                <button type="button" class="adu alo axp bkx bmz bne bnq bog" onClick={closeModal}>
-                  <span class="t">
-                    Close
-                  </span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="oc se">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
-                </button>
+              <div className="flex flex-col w-full h-full justify-start items-start">
+                <h1 className="font-semibold">Team: {selectedTeam}</h1>
               </div>
               <form className="space-y-6">
                 <div className="flex space-x-6">
@@ -283,6 +401,8 @@ const member = (length) => {
                         id="taskName"
                         name="taskName"
                         type="text"
+                        value={taskName}
+                        onChange={(e) => setTaskName(e.target.value)}
                         required
                         className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 text-lg sm:text-sm sm:leading-6"
                       />
@@ -298,9 +418,25 @@ const member = (length) => {
                         id="date"
                         name="date"
                         type="date"
+                        value={taskDate}
+                        onChange={(e) => setTaskDate(e.target.value)}
                         required
                         className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 text-lg sm:text-sm sm:leading-6"
                       />
+                    </div>
+                  </div>
+                  <div className='flex-1'>
+                    <label htmlFor="date" className="flex text-lg font-medium leading-6 dark:text-white text-gray-900 items-stretch">
+                      Assign a user
+                    </label>
+                    <div className='mt-2'>
+                    <select name='users' id='users' value={selectedUserEmail} onChange={handleUserChange} className="block w-full px-4 py-2 border rounded-lg mt-1">
+                      {users.map((user, index) => (
+                        <option key={index} value={user.email}>
+                          {user.name} ({user.email})
+                        </option>
+                      ))}
+                    </select>
                     </div>
                   </div>
                 </div>
@@ -312,14 +448,28 @@ const member = (length) => {
                     placeholder="Description of Task"
                     id="description"
                     name="description"
+                    value={taskDescription}
+                    onChange={(e) => setTaskDescription(e.target.value)}
                     required
                     className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 text-lg sm:text-sm sm:leading-6"
                   />
                 </div>
-                <button onClick={closeModal} className="mt-4 bg-sky-300 hover:bg-cyan-200 text-white font-semibold px-6 py-3 rounded">
+                <button
+                  onClick={addTask}
+                  disabled={!selectedTeam || !taskName || !taskDate || !selectedUserEmail || !taskDescription}
+                  className="mt-4 bg-sky-300 hover:bg-cyan-200 text-white font-semibold px-6 py-3 rounded"
+                >
                   Add
                 </button>
               </form>
+              <div class="aa de dn md aue avb bxo">
+                <button type="button" class="adu alo axp bkx bmz bne bnq bog" onClick={closeModal}>
+                  <span class="t">
+                    ......
+                  </span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" class="oc se">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
+                </button>
+              </div>
             </div>
           </div>
         )}
