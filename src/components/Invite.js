a70f2279fc50917eda67fc803fa3a707cccec476
knee-son <okaokaloid@gmail.com>
2023-12-06 20:10:51 +0800

    code cleanup Invite.js

--- src/components/Invite.js
+++ src/components/Invite.js
@@ -15,12 +15,13 @@ const Invite = () => {
   const [URL, setURL] = useState('');
   const [currentUser, setCurrentUser] = useState(null);
   const [isManager, setIsManager] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // Check if the user is authenticated
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         setCurrentUser(user);
+        console.log(`user:${'\t'.repeat(3)}` + user.uid);
       } else {
         setCurrentUser(null);
       }
@@ -29,40 +30,37 @@ const Invite = () => {
     return () => unsubscribe();
   }, []);
 
-  // useEffect(async () => {
-  //   try {
-  //     const userDocRef = doc(db, "users", currentUser.uid);
-  //     const userDocSnapshot = await getDoc(userDocRef);
-  
-  //     if (userDocSnapshot.exists()) {
-  //       const userData = userDocSnapshot.data();
-  //       const userRole = userData.role; // Replace 'role' with the actual field name where the role is stored
-  
-  //       if (userRole === "manager") {
-  //         setIsManager(true);
-  //         console.log(userRole);
-  //         console.log("User is a manager");
-  //       }
-  //       else if(userRole === "member"){
-  //         setIsManager(false);
-  //         console.log(userRole);
-  //         console.log("User is a member");
-  //       }
-  //     }
-  //   } catch (error) {
-  //     console.error("Error checking user role:", error);
-  //   }
+  useEffect(() => {
+    const checkUserRole = async (user) => {
+      try {
+        const userDocRef = doc(db, "users", user.uid);
+        const userDocSnapshot = await getDoc(userDocRef);
+    
+        if (userDocSnapshot.exists()) {
+          const userData = userDocSnapshot.data();
+          const userRole = userData.role;
+            if (userRole === "manager")
+              setIsManager(true);
+            else if(userRole === "member")
+              setIsManager(false);
+        }
+      } catch (error) {
+        console.error("Error checking user role:", error);
+      }
+    };
+    
+    if(currentUser)
+      checkUserRole(currentUser);
 
-  //   return () => {
-  //   };
-  // }, [isManager, currentUser.uid]);
+    return () => {};
+  }, [currentUser]);
 
   useEffect(() => {
     const fetchInviteData = async () => {
       try {
         const inviteDocRef = doc(db, 'invites', inviteId);
         const inviteSnapshot = await getDoc(inviteDocRef);
-
+  
         if (inviteSnapshot.exists()) {
           const newInviteData = {
             id: inviteSnapshot.id,
@@ -70,9 +68,7 @@ const Invite = () => {
           };
           setInviteData(newInviteData);
           setURL(`https://privo.pages.dev/login?ref=${newInviteData.id}`);
-          console.log('invite data');
-          console.log(inviteData.user);
-          console.log(currentUser.uid);
+          console.log('invite data:\t' + newInviteData.user);
         } else {
           setHasDoc(false);
           console.log('Invite document not found');
@@ -81,13 +77,16 @@ const Invite = () => {
         console.error('Error fetching invite data:', error);
       }
     };
-
-    if (inviteId) {
+  
+    if (inviteId)
       fetchInviteData();
-    }
+  
+    return () => {};
   }, [inviteId]);
 
   const addUserToTeam = async () => {
+    setLoading(true);
+
     const currentLoggedEmail = auth.currentUser.email;
     const teamInvitation = inviteData.team;
 
@@ -122,61 +121,40 @@ const Invite = () => {
   };
 
   const navigate = useNavigate();
-  const navigateToTeams = () => { navigate('/dashboard'); };
+  const navigateToTeams = () => { navigate('/teams'); };
 
   return (
     <div className="flex flex-col items-center justify-center space-y-4 h-screen bg-slate-200">
-      {inviteData ?
-        hasDoc ?
-          currentUser.uid === inviteData.user || isManager? (
-            <div className="bg-white p-8 rounded-lg shadow-lg flex flex-col items-center justify-center">
-              <h2 className="text-4xl font-semibold mb-2">Invite Details</h2>
-              <div className="mb-4 p-5">
-                <QRCode value={URL} size={200} />
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        {inviteData ?
+          hasDoc ?
+            currentUser.uid === inviteData.user || isManager? (
+              <div className='flex flex-col items-center justify-center'>
+                <h2 className="text-4xl font-semibold mb-2">{isManager ? 'Invite Details'  : 'You have been invited!'}</h2>
+                <div className="mb-4 p-5">
+                  <QRCode value={URL} size={200} />
+                </div>
+                <p className="mb-1">ID: {inviteData.id}</p>
+                <p className="mb-1">Manager: {inviteData.manager}</p>
+                <p className="mb-1">Team: {inviteData.team}</p>
+                <p className="mb-1">Time: {inviteData.time.toDate().toString()}</p>
+                <p className="mb-1">User: {inviteData.user}</p>
+                {currentUser.uid === inviteData.user ?
+                <button onClick={addUserToTeam} className="m-4 px-4 py-2 bg-blue-500 text-white rounded-md">Join Team {inviteData.team}</button> : <div/>}
+                <div className = {loading ? "animate-spin rounded-full h-10 w-10 border-t-4 border-blue-500 border-solid m-2" : ''}></div>
               </div>
-              <p className="mb-1">ID: {inviteData.id}</p>
-              <p className="mb-1">Manager: {inviteData.manager}</p>
-              <p className="mb-1">Team: {inviteData.team}</p>
-              <p className="mb-1">Time: {inviteData.time.toDate().toString()}</p>
-              <p className="mb-1">User: {inviteData.user}</p>
-              {currentUser.uid === inviteData.user ?
-              <button onClick={addUserToTeam} className="m-4 px-4 py-2 bg-blue-500 text-white rounded-md">Join team {inviteData.team}</button> : <div/>}
-            </div>
-          ): (
-          <p className="text-gray-600">Sorry, this invite isn't for you...</p>
-        ) : (
-          <p className="text-red-500">Invite does not exist!</p>
-      ) : (
-        <p className="text-gray-600">Loading invite data...</p>
-    )}
+                ): (
+                  <p className="text-gray-800">Sorry, this invite isn't for you...</p>
+                  ) : (
+            <p className="text-red-500">Invite does not exist!</p>
+            ) : (
+              <p className="text-gray-800">Loading invite data...</p>
+          )
+        }
+      </div>
     </div>
   );
 
-  return (
-    <div className="flex flex-col items-center justify-center space-y-4 h-screen bg-slate-200">
-      {inviteData ? (
-        <div className="bg-white p-8 rounded-lg shadow-lg flex flex-col items-center justify-center">
-          <div className="mb-4 p-5">
-            <QRCode value={URL} size={200} />
-          </div>
-          <h2 className="text-2xl font-semibold mb-2">Invite Details</h2>
-          <p className="mb-1">ID: {inviteData.id}</p>
-          <p className="mb-1">Manager: {inviteData.manager}</p>
-          <p className="mb-1">Team: {inviteData.team}</p>
-          <p className="mb-1">Time: {inviteData.time.toDate().toString()}</p>
-          <p className="mb-1">User: {inviteData.user}</p>
-        </div>
-      ) : hasDoc ?
-          currentUser.id === inviteData.id ? (
-              <p className="text-gray-600">Sorry, this invite isn't for you...</p>
-            )
-          : (
-        <p className="text-gray-600">Loading invite data...</p>
-      ) : (
-        <p className="text-red-500">Invite does not exist!</p>
-      )}
-    </div>
-  );
 };
 
 export default Invite;
